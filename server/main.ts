aba7f1b1dcdb9c09f2c11364b7fd744dcca95fde
clianor <ejrgus94160@gmail.com>
2021-01-21 21:49:46 +0900

    ValidationPipe 셋팅
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- server/main.ts
+++ server/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import register from '@react-ssr/nestjs-express/register';
@@ -5,6 +6,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
   await register(app);
   await app.listen(3000, async () => {
     console.log(`> Ready on http://localhost:3000`);
